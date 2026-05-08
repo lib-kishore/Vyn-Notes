9a1d522c5d96e2322b7cbccc5c6db732316a3ccd
Kishore M <kishoredxd@gmail.com>
2026-05-08 20:14:33 +0530

    Add TLS certificate validation option for MongoDB

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ load_dotenv(BASE_DIR / ".env")
 MONGODB_URI = st.secrets.get("MONGODB_URI") or os.getenv("MONGODB_URI")
 GROQ_API_KEY = st.secrets.get("GROQ_API_KEY") or os.getenv("GROQ_API_KEY")
 
-MONGODB_DB = st.secrets.get("MONGODB_DB", "vynnotes")
+MONGODB_DB = st.secrets.get("MONGODB_DB", "vynnotes") 
+MONGODB_TLS_ALLOW_INVALID_CERTS = st.secrets.get("MONGODB_TLS_ALLOW_INVALID_CERTS", "false").lower() in ("1", "true", "yes")
 MONGODB_NOTES_COLLECTION = st.secrets.get("MONGODB_NOTES_COLLECTION", "notes")
 MONGODB_CHATS_COLLECTION = st.secrets.get("MONGODB_CHATS_COLLECTION", "chats")
